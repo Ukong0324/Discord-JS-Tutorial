3edaa38964a8af3619a3ced084d63b27df54bd85
제스퍼 <41337427+Ukong0324@users.noreply.github.com>
2020-05-27 22:01:48 +0900

    3화 설명추가

--- 3화/bot.js
+++ 3화/bot.js
@@ -28,14 +28,29 @@ function runCommand(command, message, args, prefix) { // 명령어를 실행할 
 }
 client.on("message", async msg => {
     const prefix = "튜토야 " // 접두사(prefix)를 저는 튜토야 라고 선정하였습니다!
-    if(!msg.content.startsWith(prefix)) return // 만약에 메세지 내용이 접두사로 시작하지 않으면 return
+    if (!msg.content.startsWith(prefix)) return // 만약에 메세지 내용이 접두사로 시작하지 않으면 return
     let args = msg.content.slice(prefix.length).trim().split(/ +/g) // argument
     let command = args.shift().toLowerCase() // 커멘드에 대한 이름을 선언
     try { // 아래의 명령어를 시도합니다.
-    runCommand(command, msg, args, prefix) // 위에서 함수로 선언한 것을 command, msg(message), args(argument), prefix를 불러와 명령어를 실행
+        runCommand(command, msg, args, prefix) // 위에서 함수로 선언한 것을 command, msg(message), args(argument), prefix를 불러와 명령어를 실행
     } catch (e) {
-       console.error(e) // 명령어를 실행하는 도중 에러가 발생하였다고 알려줍시다.
+        console.error(e) // 명령어를 실행하는 도중 에러가 발생하였다고 알려줍시다.
     }
 })
 
-client.login(config.token) // config에 token 부분을 불러오게 합시다.
+client.login(config.token) // config에 token 부분을 불러오게 합시다.}
+
+/**
+ * 이제 Commands 폴더를 생성하고 Bot, Moderator 폴더를 생성후 명령어 소스는 아래와 같이 사용하시면 됩니다.
+   exports.run = async (client, msg, args, prefix) => {
+    // 여기 부분에 평소에 작성하던 코드를 이쪽에서 해주시면 되요!
+}
+
+exports.config = {
+    name: 'name', // 말 그대로 봇 명령어 이름이에요
+    aliases: ['aliases', 'aliases1', 'aliases2'], // 여기는 위에 봇 명령어 이름을 aliases, aliases1, aliases2 처럼 적은거대로 명령어를 실행하면 위에 코드가 작동해요
+    category: ['bot'], // 여기는 카테고리에요. 자신이 원하는데로 카테고리를 분류해주시면 될꺼 같아요.
+    des: ['config 설정방법'], // 여기는 해당 명령어에 대한 설명입니다.
+    use: ['튜토야 exports 사용법'] // 당신이 원한다면 사용 방법을 적어주시면 다른 유저분들께 도움이 될꺼에요 (추후에 help 명령어 만들예정)
+}
+ */
