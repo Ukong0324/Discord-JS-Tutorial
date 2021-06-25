a8aae1ed16f965e95c42646fcfa1b8ac7caaf94f
Youbin Choi <41337427+Ukong0324@users.noreply.github.com>
2021-06-25 20:41:02 +0900

    Update bot.js

--- 3화/bot.js
+++ 3화/bot.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const fs = require('fs') // 우리는 커멘드 핸들러를 만들기 위해서 fs 라는 모듈을 사용하였습니다! "npm i fs --save" 
+const fs = require('fs') 
 const config = require("./config.json") // config를 선언하여 config.json이 필요하다고 선언하였습니다.
 client.on("ready", () => {
     console.log(`${client.user.tag} 봇에 로그인 하였습니다!`);
